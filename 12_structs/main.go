f0ff3f882fc617658224b588da3693dfe4b86b89
joey <joey@uppercut.internal>
2018-12-21 16:29:03 -0600

    added latest tutorial files

--- 12_structs/main.go
+++ 12_structs/main.go
@@ -6,19 +6,41 @@ import (
 )
 
 type Person struct {
-	firstName string
-	lastName  string
-	city      string
-	gender    string
+	// firstName string
+	// lastName  string
+	// city      string
+	// gender    string
+	firstName, lastName, city, gender string
 	age       int
 }
 
+// greeting method (value receiver)
 func (p Person) greet() string {
-	return "Hi " + p.firstName + " " + p.lastName + "!! According to our records you're a " + strconv.Itoa(p.age) + " year old " + p.gender + " living in " p.city + "."
+	return "Hi " + p.firstName + " " + p.lastName + "! According to our records you're a " + strconv.Itoa(p.age) + " year old " + p.gender + " living in " + p.city + "."
+}
+
+//hasBirthday method (pointer receiver )
+func (p *Person) hasBirthday() {
+	p.age++
+}
+
+//getMarried method (pointer receiver)
+
+func (p *Person) getMarried(spouseLastName string) {
+	if p.gender == "male" {
+		return
+	} else {
+		p.lastName = spouseLastName
+	}
 }
 
 func main() {
-	// person1 := Person{firstName: "Samantha", lastName: "Mathis", city: "Austin", gender: "shemale", age: 300}
-	person1 := Person{"Joey", "Douglas", "Austin", "male", 42}
+	person1 := Person{firstName: "Joey", lastName: "Douglas", city: "Austin", gender: "male", age: 42}
+	person2 := Person{"Cindy", "Garcia", "Austin", "female", 36}
+	person1.hasBirthday()
+	person1.getMarried("Garcia")
+	person2.getMarried("Douglas")
 	fmt.Println(person1.greet())
+	fmt.Println(person2.greet())
+
 }
